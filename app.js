0a407735d645ab1c0f07b7eb5789ca72cd2ea046
Marcos Augusto de Carvalho Freire <augustofreire@gmail.com>
2019-10-20 23:00:05 -0700

    Project files just added

---
+++ app.js
@@ -0,0 +1,15 @@
+//const variable = require("jsmodule");
+
+/**
+ * Renders chart into demo page
+ * @param {array of numbers} data - the data the chart should work
+ * from Start with just an Array of numbers
+ * e.g. [1, 2, 3, 4, 5]
+ * @param {object} options - should be an object which has options for the chart.
+ * e.g. width and height of the bar chart
+ * @param {Dom / jQuery element} element - element that the chart will get rendered into
+ * the page
+  */
+/*function drawBarChart(data, options, element){
+
+}*/
